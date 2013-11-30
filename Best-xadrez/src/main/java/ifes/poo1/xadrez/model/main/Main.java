d0f7a68571fd74114ec3c9d3a5487023c86d4836
pnakibar <pedronakibar@gmail.com>
2013-11-29 22:55:43 -0200

    Ultimo Commit antes do merge com o master

--- Best-xadrez/src/main/java/ifes/poo1/xadrez/model/main/Main.java
+++ Best-xadrez/src/main/java/ifes/poo1/xadrez/model/main/Main.java
@@ -2,12 +2,16 @@ package ifes.poo1.xadrez.model.main;
 
 import ifes.poo1.xadrez.control.Control;
 
-public class Main {
 
+public class Main {
+    
+        
+    
+    
 	public static void main(String[] args) {
 		Control control = new Control();
 		control.controlarPeca("0605");
-                control.controlarPeca("0506");
+                
                
 		control.ImprimeTab();
 
